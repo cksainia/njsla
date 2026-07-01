579a36220209b98dc6ffc29bd5a9e6e4e1177f8a
cksainia <chitts@gmail.com>
2026-07-01 02:35:59 -0400

    QA fixes: unscored writing no longer shows a bogus level or pollutes dashboard; Back disabled at item 1 of each session; multi-select requires exact count; writing rows in dashboard; SW cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'njsla-a-v1';
+const CACHE = 'njsla-a-v2';
 const ASSETS = ['./', './index.html', './data.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
